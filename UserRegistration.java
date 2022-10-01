aafed762584dfc68559b9803fba37d58d8326ce7
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-01 20:03:59 +0530

    Added : Functionality Of Lambda Expression To Validate Data

--- UserRegistration.java
+++ UserRegistration.java
@@ -37,80 +37,10 @@ public class UserRegistration {
             if (!result)
                 throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_INVALID,
                         "Please Give Valid Entry");
-            return result;
+            return true;
         } catch (NullPointerException exception) {
             throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL,
                     "Entry Should Be Not null ");
-        }
-    }
-
-    public void firstNameValidate() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Your First Name");
-        String name = sc.next();
-
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,20}$");
-        Matcher matcher = pattern.matcher(name);
-        if (matcher.matches()) {
-        } else {
-            System.out.println("Given First Name Is InValid");
-        }
-    }
-
-    public void lastNameValidate() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your last name");
-        String name = sc.next();
-
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,20}$");
-        Matcher matcher = pattern.matcher(name);
-        if (matcher.matches()) {
-        } else {
-            System.out.println("Given Last Name Is InValid");
-        }
-    }
-
-    public void emailValidate() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Your Email Id");
-        String email = sc.next();
-
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
-        Matcher matcher = pattern.matcher(email);
-        if (matcher.matches()) {
-        } else {
-            System.out.println("Given Email Id Is InValid");
-        }
-    }
-
-    public void mobileValidate() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Your Mobile Number");
-        String mobile = sc.nextLine();
-
-        Pattern pattern = Pattern.compile("^[0-9]{0,2}[ ][0-9]{10}");
-        Matcher matcher = pattern.matcher(mobile);
-        if (matcher.matches()) {
-        } else {
-            System.out.println("Given Mobile Number Is InValid");
-        }
-    }
-
-    public void passwordValidate() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Your Password");
-        String password = sc.next();
-
-        Pattern pattern = Pattern.compile("^(?=.{8,20})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$");
-        Matcher matcher = pattern.matcher(password);
-        if (matcher.matches()) {
-        } else {
-            System.out.println("Given Password Is InValid");
         }
     }
 }
--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,11 +1,95 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface DataValidate {
+    void Validator();
+}
+
 public class UserRegistrationMain {
 
     public static void main(String[] args) {
-        UserRegistration userRegistration = new UserRegistration();
-        userRegistration.firstNameValidate();
-        userRegistration.lastNameValidate();
-        userRegistration.emailValidate();
-        userRegistration.mobileValidate();
-        userRegistration.passwordValidate();
+        // Lambda Expression ->
+        DataValidate firstName = () -> {
+            Scanner sc = new Scanner(System.in);
+
+            String regex = "^[A-Z]{1}[a-z]{3,20}$";
+
+            System.out.println("Enter your First name");
+            String name = sc.next();
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(name);
+            if (matcher.matches()) {
+                System.out.println("Given First Name Is Valid");
+            } else {
+                System.out.println("Given First Name Is InValid");
+            }
+        };
+        firstName.Validator();
+
+        DataValidate lastName = () -> {
+            Scanner sc = new Scanner(System.in);
+
+            String regex = "^[A-Z]{1}[a-z]{3,20}$";
+
+            System.out.println("Enter your last name");
+            String name = sc.next();
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(name);
+            if (matcher.matches()) {
+                System.out.println("Given Last Name Is Valid");
+            } else {
+                System.out.println("Given Last Name Is InValid");
+            }
+        };
+        lastName.Validator();
+
+        DataValidate eMail = () -> {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter Your Email Id");
+            String email = sc.next();
+
+            Pattern pattern = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.matches()) {
+            } else {
+                System.out.println("Given Email Id Is InValid");
+            }
+        };
+        eMail.Validator();
+
+        DataValidate Mobile = () -> {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter Your Mobile Number");
+            String mobile = sc.nextLine();
+
+            Pattern pattern = Pattern.compile("^[0-9]{0,2}[ ][0-9]{10}");
+            Matcher matcher = pattern.matcher(mobile);
+            if (matcher.matches()) {
+            } else {
+                System.out.println("Given Mobile Number Is InValid");
+            }
+        };
+        Mobile.Validator();
+
+        DataValidate Password = () -> {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter your Password");
+            String password = sc.next();
+
+            Pattern pattern = Pattern.compile("^(?=.{8,20})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$");
+            Matcher matcher = pattern.matcher(password);
+            if (matcher.matches()) {
+            } else {
+                System.out.println("Given Password Is InValid");
+            }
+        };
+        Password.Validator();
     }
 }
