26dafd67b787543a6f620bbd862f1f5cb6a98567
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-01 19:28:24 +0530

    Added : Functionality To Check Valid Email Id Pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,12 +3,17 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+[.+-]?[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]+[.]?[a-zA-Z]+[,]?$";
     public boolean validateFirstName(String firstName) {
         return patternChecker(firstName, NAME_PATTERN);
     }
 
     public boolean validateLastName(String lastName) {
         return patternChecker(lastName, NAME_PATTERN);
+    }
+
+    public boolean validateEmailId(String emailId) {
+        return patternChecker(emailId, EMAIL_PATTERN);
     }
 
     public boolean patternChecker(String input,String fieldPattern) {
