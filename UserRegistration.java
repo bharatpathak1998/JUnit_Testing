a14d798d26fb35e9d791f89f6303362471a2fe5d
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-01 19:42:51 +0530

    Added : Functionality To Check Valid All Sample Email Pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,28 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
-    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+[.+-]?[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]+[.]?[a-zA-Z]+[,]?$";
-    public static final String MOBILE_PATTERN = "^[0-9]{0,2}[ ][0-9]{10}";
-    public static final String PASSWORD_PATTERN = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
-    public boolean validateFirstName(String firstName) {
-        return patternChecker(firstName, NAME_PATTERN);
-    }
-
-    public boolean validateLastName(String lastName) {
-        return patternChecker(lastName, NAME_PATTERN);
-    }
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+[.+-]?[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]+[.]?[a-zA-Z]+[,]?$";
 
     public boolean validateEmailId(String emailId) {
         return patternChecker(emailId, EMAIL_PATTERN);
-    }
-
-    public boolean validateMobileNumber(String mobileNumber) {
-        return patternChecker(mobileNumber, MOBILE_PATTERN);
-    }
-
-    public boolean validatePassword(String password) {
-        return patternChecker(password, PASSWORD_PATTERN);
     }
 
     public boolean patternChecker(String input,String fieldPattern) {
